71fe869e6a96f58fb2ef39c43ffe2a442ea0d7f5
Zacck <coderv63@gmail.com>
2016-11-12 07:57:44 +0200

    work on adding react libs and setting up boilerplate

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+//require libs
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+//load foundation
+$(document).foundation();
+
+ReactDOM.render(
+  <div>
+    <p> Hello world React</p>
+  </div>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ var webpack = require('webpack');
 var path = require('path');
 var envFile = require('node-env-file');
 
-
-//create enviroment variable for dev
+//enviroment variable
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+//load env file using process.env
 try {
-  envFile(path.join(__dirname, 'config'+process.env.NODE_ENV +'.env'));
+  envFile(path.join(__dirname, 'config/'+process.env.NODE_ENV + '.env'));
 } catch(e) {
 
 }
 
-//export webpack config
-module.exports = {
+module.exports =  {
+  //find this file and start from there
   entry: [
     'script!jquery/dist/jquery.min.js',
     'script!foundation-sites/dist/foundation.min.js',
@@ -24,8 +24,8 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      '$': 'jquery',
-      'jQuery': 'jquery'
+      '$':'jquery',
+      'jQuery':'jquery'
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
@@ -33,28 +33,48 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-          'process_env': {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-            API_KEY: JSON.stringify(process.env.API_KEY),
-            AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
-            DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
-            STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
-          }
+      'process_env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
+      }
     })
   ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
+  resolve: {
+    //where all this needs to happen
+    root: __dirname,
+    modulesDirectories:[
+      'node_modules',
+      './app/components',
+      './app/components/forms',
+      './app/api'
+    ],
+    alias: {
+      app: 'app',
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx',
+      configureStore: 'app/store/configureStore.jsx'
+    },
+    //files we want to process
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [
       {
+        //name of loader
         loader: 'babel-loader',
-        query: {
+        query: { //parse the files through react
           presets: ['react', 'es2015', 'stage-0']
         },
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/
+        test: /\.jsx?$/, //regex for .jsx extension
+        exclude: /(node_modules|bower_components)/ //set up folders to be ignored
       }
     ]
   },
