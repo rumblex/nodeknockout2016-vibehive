123eaf508d768e9d3bd0b3ba68166a3f4c2a6c8b
Zacck <coderv63@gmail.com>
2016-11-12 21:11:42 +0200

    add activity form

--- src/components/forms/ActivityForm.jsx
+++ src/components/forms/ActivityForm.jsx
@@ -4,19 +4,21 @@ import {connect} from 'react-redux'
 export class ActivityForm extends Component {
   render() {
     return (
-      <div className="activity-form">
+      <div className="activity-form small-centered small-12">
       <div>
         <p> create a vibe </p>
       </div>
       <div>
-        <form>
+        <form >
           <label>VIBE NAME:</label>
           <input type="text" />
           <label>LOCATION: </label>
           <input type="text"/>
-          <input type="date-time"/>
-          <button> Upload </button>
-          <button> CREATE VIBE</button>
+          <label>TIME:</label>
+          <input type="datetime"/>
+          <input ref="file" type="file" name="file" className="button upload-file"/>
+          <button className="button"> Upload </button>
+          <button className="button"> CREATE VIBE</button>
         </form>
       </div>
       </div>
