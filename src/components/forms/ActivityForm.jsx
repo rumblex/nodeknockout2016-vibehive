516233752e8a53c7b1bdf1b87c91f0e45235fa12
Zacck <coderv63@gmail.com>
2016-11-12 22:34:15 +0200

    add function prototypes for activity submissiion

--- src/components/forms/ActivityForm.jsx
+++ src/components/forms/ActivityForm.jsx
@@ -1,7 +1,33 @@
 import React , {Component} from 'react'
 import {connect} from 'react-redux'
-
+import * as actions from 'actions'
+import HiveApi from 'HiveApi'
 export class ActivityForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleImage = this.handleImage.bind(this);
+    this.state = {
+      file: null
+    };
+  }
+  componentDidMount () {
+
+  }
+  handleImage(Event) {
+    Event.preventDefault();
+    let file = e.target.files[0];
+    this.setState({
+        file: file,
+    });
+  }
+  submitActivity(Event) {
+    Event.preventDefault();
+    //grab image and hold
+
+    //persist activity
+    //tag image and upload with activity
+
+  }
   render() {
     return (
       <div className="activity-form small-centered small-12">
@@ -16,7 +42,7 @@ export class ActivityForm extends Component {
           <input type="text"/>
           <label>TIME:</label>
           <input type="datetime"/>
-          <input ref="file" type="file" name="file" className="button upload-file"/>
+          <input ref="file" type="file" name="file" s className="button upload-file"/>
           <button className="button"> Upload </button>
           <button className="button"> CREATE VIBE</button>
         </form>
