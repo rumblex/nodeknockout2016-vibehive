8b0b2ccc269d791851378b1cfd5115c9a9800d47
Zacck <coderv63@gmail.com>
2016-11-12 15:41:16 +0200

    work on styling for activity item

--- src/components/app/ActivityItem.jsx
+++ src/components/app/ActivityItem.jsx
@@ -5,16 +5,19 @@ export class ActivityItem extends Component {
   render() {
     return (
       <div className="activity-item">
-        <div>
-          <p className="activity-time"> 6AM, 24 Nov</p>
-          <p className="acticity-title"> Hike Lion's Head </p>
-          <p className="locale-title"> Cape Town </p>
-        </div>
-        <div>
-          <button> String</button>
-        </div>
-
-        <img className="activity-image"/>
+        <div className="row">
+          <div className="columns small-9 medium-9 large-9">
+            <p className="activity-time"> 6AM, 24 Nov</p>
+            <p className="acticity-title"> Hike Lion's Head </p>
+            <p className="locale-title"> Cape Town </p>
+          </div>
+          <div className="columns small-3 medium-3 large-3">
+            <button className="sting-button button"> STING!</button>
+         </div>
+       </div>
+       <div className="row">
+         <img src="images/" className="activity-image"/>
+       </div>
       </div>
     )
   }
