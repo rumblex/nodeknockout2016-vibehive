0e178728ef143ce6221faf6513555f9f770df1e6
Zacck <coderv63@gmail.com>
2016-11-12 17:23:47 +0200

    work on actions for activity

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,17 +1,17 @@
 //importing firebase we can avoid the filename since its called index :)
-import firebase, {firebaseRef, geoFire} from 'src/firebase/';
+import firebase, {firebaseRef, geoFire, storageRef} from 'src/firebase/';
 
 //start an asychronous call to load categories from firebase, then add to local state upon request return
 export var startLoadCategories = () => {
 	return (dispatch, getState) => {
 
-    firebaseRef.child('categories').once('value').then(function(snapshot) {
+    firebaseRef.child('categories').once('value').then((snapshot) =>  {
 	  console.log(snapshot.val());
 	  //add categories to local store
 	  dispatch(loadAllCategories(snapshot.val()));
 	});
 
-    
+
   }
 }
 
@@ -19,5 +19,26 @@ export var loadAllCategories = (categories) => {
 	return {
 	    type: 'LOAD_ALL_CATEGORIES',
 	    categories
+	}
+}
+
+export var loadActivities = (activities) => {
+	return {
+		type:  'LOAD_ACTIVITIES',
+		activities
+	};
+}
+
+export var StartLoadActivities = () => {
+	//get list of user activities
+
+	//get activities that are close enough
+
+	//that are in the user activities
+
+	return (dispatch, getState) => {
+		firebaseRef.child('activities').once('value'.then((snapshot) => {
+			dispatch(loadActivities(snapshot.val()))
+		});
 	}
 }
