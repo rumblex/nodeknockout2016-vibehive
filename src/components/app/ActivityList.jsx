2354311f295920e7db697ecf620ff6065715a1a0
Zacck <coderv63@gmail.com>
2016-11-12 14:16:00 +0200

    render activity list with place holder items

--- src/components/app/ActivityList.jsx
+++ src/components/app/ActivityList.jsx
@@ -1,11 +1,40 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import ActivityItem from 'ActivityItem'
 
 export class ActivityList extends Component {
   render(){
+    var renderActivities = () =>{
+      var acts =  [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        },
+        {
+          id: 3
+        },
+        {
+          id: 4
+        }
+      ];
+      if(acts.length === 0) {
+        return(
+          <p> No activities Yet</p>
+        );
+      }
+      return acts.map((act) => {
+        return(
+          <ActivityItem key={act.id} {...act}/>
+        );
+      })
+    }
     return(
-      <p> List Here </p>
-    )
+      <div>
+        {renderActivities()}
+      </div>
+    );
   }
 }
 
